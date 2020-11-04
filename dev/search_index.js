607aec6e99a9ccedc06e5d1ec47dec6e859ae82f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-11-04 10:30:09 +0000

    build based on 7263396

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimplePNGs","category":"page"},{"location":"#SimplePNGs","page":"Home","title":"SimplePNGs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimplePNGs]","category":"page"}]
+}
